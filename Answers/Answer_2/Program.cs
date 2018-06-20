70b74aa5f9d69d606206dadd4824d19e698306b7
SakinaSadigova <36983955+SakinaSadigova@users.noreply.github.com>
2018-06-20 06:06:10 -0700

    The answer to question 2 added.

---
+++ Answers/Answer_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Answer_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number :");
+            string text =Console.ReadLine();
+            int number;
+            if (int.TryParse(text,out number))
+            {
+                char[] digits = text.ToCharArray();
+                char firstDigit;
+                firstDigit = digits[0];
+                digits[0] = digits[digits.Length - 1];
+                digits[digits.Length - 1]=firstDigit;
+                text = new string(digits);
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.WriteLine("Could not be convert to valid number");
+            }
+            
+        }
+    }
+}
