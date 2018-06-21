b9ccb38977843837ef46990a3d4be8f9be856cf8
Qonaq <asakina.p302@code.edu.az>
2018-06-21 19:16:10 +0400

    The answer to question 6(Not Full)

---
+++ Answers/Answer_3/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Answer_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxList = new System.Windows.Forms.CheckedListBox();
+            this.defineBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBoxList
+            // 
+            this.checkBoxList.CheckOnClick = true;
+            this.checkBoxList.FormattingEnabled = true;
+            this.checkBoxList.Items.AddRange(new object[] {
+            "Halsızlıq ",
+            "Kitab oxumağa həvəssizlik",
+            "Boğazda qıcıqlanma ",
+            "Öskürək",
+            "Yuxusuzluq"});
+            this.checkBoxList.Location = new System.Drawing.Point(12, 12);
+            this.checkBoxList.Name = "checkBoxList";
+            this.checkBoxList.Size = new System.Drawing.Size(237, 109);
+            this.checkBoxList.TabIndex = 0;
+            // 
+            // defineBtn
+            // 
+            this.defineBtn.Location = new System.Drawing.Point(174, 127);
+            this.defineBtn.Name = "defineBtn";
+            this.defineBtn.Size = new System.Drawing.Size(75, 23);
+            this.defineBtn.TabIndex = 1;
+            this.defineBtn.Text = "Teyin et";
+            this.defineBtn.UseVisualStyleBackColor = true;
+            this.defineBtn.Click += new System.EventHandler(this.defineBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 178);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 447);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.defineBtn);
+            this.Controls.Add(this.checkBoxList);
+            this.Name = "Form1";
+            this.Text = "Symptoms";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkBoxList;
+        private System.Windows.Forms.Button defineBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Answers/Answer_3/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using Answer_3.Models;
+namespace Answer_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void defineBtn_Click(object sender, EventArgs e)
+        {
+            
+            DiagnosisDbEntities diagnosisDbEntities = new DiagnosisDbEntities();
+            foreach (string item in checkBoxList.CheckedItems)
+            {
+               
+            }
+            var tagIds = new int[] { 1, 2,3,4,5};
+
+            var query= from illness in diagnosisDbEntities.Illnesses
+                        from symptom in illness.Symptoms
+                        where tagIds.Contains(symptom.Id)
+                        select symptom.Id;
+//           SELECT* FROM IllnessSymptom ilssym
+//INNER JOIN Illnesses ill ON ilssym.Illnesses_id = ill.id
+//WHERE ilssym.Symptoms_Id IN(2,3,4)
+            dataGridView1.DataSource = query.ToList();
+
+        }
+    }
+}
